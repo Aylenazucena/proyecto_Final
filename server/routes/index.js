2a8d104a9757f591af9a3a1813bb10a23affcf3e
Perezbere <152465212+Perezbere@users.noreply.github.com>
2023-12-03 23:50:40 +0000

    🤞Config winston se realizo la configuracion de winston

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,12 +1,24 @@
-const express = require('express');
+import express from 'express';
 
-const router = express.Router();
+const { Router } = express;
+
+const router = Router();
 
 /* GET home page. */
 router.get('/', (req, res) => {
-  res.render('index', { title: 'ITGAM', author: 'espinoza aylen & perez berenice' });
+  const iconSet = ['⭐', '🤖', '🍉'];
+  const icon = iconSet[Math.floor(Math.random() * 3)];
+  res.render('index', { title: 'proyecto final', icon });
 });
 
-// Pripio
+router.get('/author', (req, res) => {
+  // Creating a View-Model
+  const author = {
+    name: 'aylen & berenice',
+    lastname: 'espinoza & perez',
+  };
+  // Sending the view-model to be rendered by a View
+  res.render('author', author);
+});
 
-module.exports = router;
+export default router;
